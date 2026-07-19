ce8cc362ff5bf8f5e2a2166ef69a1656851b8520
Muhammad Ghulam Ali <mghulamali888@gmail.com>
2026-07-19 15:52:45 +0500

    Add IQR outlier detection and skewness testing (utils.py functions), boxplots for income/credit/annuity

---
+++ src/utils.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# Outlier Detection
+def outlier_detection(df, column_name):
+    Q1 = df[column_name].quantile(0.25)
+    Q3 = df[column_name].quantile(0.75)
+
+    IQR = Q3 - Q1
+
+    lower_bound = Q1 - IQR * 1.5
+    upper_bound = Q3 + IQR * 1.5
+
+    df[column_name + '_OUTLIER'] = np.where((df[column_name] > upper_bound) | (df[column_name] < lower_bound), 'Outlier', 'Normal')
+
+    return df[column_name + '_OUTLIER'].value_counts(normalize=True).round(2)
+
+# Skewness test
+def skewness_test(df, column_name):
+    skewness = float(df[column_name].skew().round(2))
+    return skewness
